871fc178934ad104a4220ce99689a9f625a45d10
theophilus doku <theodoku89@gmail.com>
2022-12-04 20:39:31 +0000

    added node to eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
     "env": {
+        "node": true,
         "browser": true,
-        "es2021": true
+        "es2021": true,
     },
     "extends": "eslint:recommended",
     "overrides": [
